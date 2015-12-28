b7cfc29e8bb39188fae1d78be0fbdf75cdc6d2a8
Marcel Klehr <mklehr@gmx.net>
2015-12-28 22:27:34 +0100

    Switch from cb-style to promises

--- index.js
+++ index.js
@@ -3,142 +3,168 @@ var request = require('superagent')
 module.exports = function(root_url, API_key) {
   var api = {
     document: {
-      create: function(type, cb) {
-        request
-        .post(root_url+'/api/v1/documents')
-        .send({type: type})
-        .set('Authorization', 'token '+API_key)
-        .end(function loadDocument(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
+      create: function(type) {
+        return new Promise(function(resolve, reject) {
+          request
+          .post(root_url+'/api/v1/documents')
+          .send({type: type})
+          .set('Authorization', 'token '+API_key)
+          .end(function (er, res) {
+            if(er) return reject(er)
+            if(res.status != 200) return reject(res.toError())
+            resolve(res.body)
+          })
         })
       }
-    , get: function(id, cb) {
-        request
-        .get(root_url+'/api/v1/documents/'+id)
-        .set('Authorization', 'token '+API_key)
-        .end(function loadDocument(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
+    , get: function(id) {
+        return new Promise(function(resolve, reject) {
+          request
+          .get(root_url+'/api/v1/documents/'+id)
+          .set('Authorization', 'token '+API_key)
+          .end(function loadDocument(err, res) {
+            if(err) return reject(err)
+            if(res.status != 200) return reject(res.toError())
+            resolve(res.body)
+          })
         })
       }
-    , delete: function(id, cb) {
-        request
-        .delete(root_url+'/api/v1/documents/'+id)
-        .set('Authorization', 'token '+API_key)
-        .end(function loadDocument(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
+    , delete: function(id) {
+        return new Promise(function(resolve, reject) {
+          request
+          .delete(root_url+'/api/v1/documents/'+id)
+          .set('Authorization', 'token '+API_key)
+          .end(function loadDocument(err, res) {
+            if(err) return reject(err)
+            if(res.status != 200) return reject(res.toError())
+            resolve(res.body)
+          })
         })
       }
-    , getSnapshots: function(id, cb) {
-        request
-        .get(root_url+'/api/v1/documents/'+id+'/snapshots')
-        .set('Authorization', 'token '+API_key)
-        .end(function(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
+    , getSnapshots: function(id) {
+        return new Promise(function(resolve, reject) {
+          request
+          .get(root_url+'/api/v1/documents/'+id+'/snapshots')
+          .set('Authorization', 'token '+API_key)
+          .end(function(err, res) {
+            if(err) return reject(err)
+            if(res.status != 200) return reject(res.toError())
+            resolve(res.body)
+          })
         })
       }
-    , getSnapshotsSince: function(id, since, cb) {
-        request
-        .get(root_url+'/api/v1/documents/'+id+'/snapshots?since='+since)
-        .set('Authorization', 'token '+API_key)
-        .end(function(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
+    , getSnapshotsSince: function(id, since) {
+        return new Promise(function(resolve, reject) {
+          request
+          .get(root_url+'/api/v1/documents/'+id+'/snapshots?since='+since)
+          .set('Authorization', 'token '+API_key)
+          .end(function(err, res) {
+            if(err) return reject(err)
+            if(res.status != 200) return reject(res.toError())
+            resolve(res.body)
+          })
         })
       }
-    , change: function(id, cs, parent, user, cb) {
-        request
-        .post(root_url+'/api/v1/documents/'+id+'/snapshots')
-        .send({changes: cs, parent: parent, user: user})
-        .set('Authorization', 'token '+API_key)
-        .end(function(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
+    , change: function(id, cs, parent, user) {
+        return new Promise(function(resolve, reject) {
+          request
+          .post(root_url+'/api/v1/documents/'+id+'/snapshots')
+          .send({changes: cs, parent: parent, user: user})
+          .set('Authorization', 'token '+API_key)
+          .end(function(err, res) {
+            if(err) return reject(err)
+            if(res.status != 200) return reject(res.toError())
+            resolve(res.body)
+          })
         })
       }
     }
   , user: {
-      create: function(body, cb) {
-          request
-          .post(root_url+'/api/v1/users')
-          .send(body)
-          .set('Authorization', 'token '+API_key)
-          .end(function loadDocument(err, res) {
-            if(err) return cb(err)
-            if(res.status != 200) return cb(res.toError())
-            cb(null, res.body)
+      create: function(body) {
+          return new Promise(function(resolve, reject) {
+            request
+            .post(root_url+'/api/v1/users')
+            .send(body)
+            .set('Authorization', 'token '+API_key)
+            .end(function loadDocument(err, res) {
+              if(err) return reject(err)
+              if(res.status != 200) return reject(res.toError())
+              resolve(res.body)
+            })
           })
         }
-      , get: function(id, cb) {
-          request
-          .get(root_url+'/api/v1/users/'+id)
-          .set('Authorization', 'token '+API_key)
-          .end(function loadDocument(err, res) {
-            if(err) return cb(err)
-            if(res.status != 200) return cb(res.toError())
-            cb(null, res.body)
+      , get: function(id) {
+          return new Promise(function(resolve, reject) {
+            request
+            .get(root_url+'/api/v1/users/'+id)
+            .set('Authorization', 'token '+API_key)
+            .end(function loadDocument(err, res) {
+              if(err) return reject(err)
+              if(res.status != 200) return reject(res.toError())
+              resolve(res.body)
+            })
           })
         }
-      , update: function(id, body, cb) {
-          request
-          .put(root_url+'/api/v1/users/'+id)
-          .send(body)
-          .set('Authorization', 'token '+API_key)
-          .end(function loadDocument(err, res) {
-           if(err) return cb(err)
-           if(res.status != 200) return cb(res.toError())
-           cb(null, res.body)
+      , update: function(id, body) {
+          return new Promise(function(resolve, reject) {
+            request
+            .put(root_url+'/api/v1/users/'+id)
+            .send(body)
+            .set('Authorization', 'token '+API_key)
+            .end(function loadDocument(err, res) {
+             if(err) return reject(err)
+             if(res.status != 200) return reject(res.toError())
+             resolve(res.body)
+            })
           })
         }
-      , delete: function(id, cb) {
-          request
-          .delete(root_url+'/api/v1/users/'+id)
-          .set('Authorization', 'token '+API_key)
-          .end(function loadDocument(err, res) {
-            if(err) return cb(err)
-            if(res.status != 200) return cb(res.toError())
-            cb(null, res.body)
+      , delete: function(id) {
+          return new Promise(function(resolve, reject) {
+            request
+            .delete(root_url+'/api/v1/users/'+id)
+            .set('Authorization', 'token '+API_key)
+            .end(function loadDocument(err, res) {
+              if(err) return reject(err)
+              if(res.status != 200) return reject(res.toError())
+              resolve(res.body)
+            })
           })
         }
-      , getDocuments: function(id, cb) {
-        request
-        .get(root_url+'/api/v1/users/'+id+'/documents')
-        .set('Authorization', 'token '+API_key)
-        .end(function(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
-        })
+      , getDocuments: function(id) {
+          return new Promise(function(resolve, reject) {
+            request
+            .get(root_url+'/api/v1/users/'+id+'/documents')
+            .set('Authorization', 'token '+API_key)
+            .end(function(err, res) {
+              if(err) return reject(err)
+              if(res.status != 200) return reject(res.toError())
+              resolve(res.body)
+            })
+          })
       }
-      , getSnapshots: function(id, cb) {
-        request
-        .get(root_url+'/api/v1/users/'+id+'/snapshots')
-        .set('Authorization', 'token '+API_key)
-        .end(function(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
-        })
+      , getSnapshots: function(id) {
+          return new Promise(function(resolve, reject) {
+            request
+            .get(root_url+'/api/v1/users/'+id+'/snapshots')
+            .set('Authorization', 'token '+API_key)
+            .end(function(err, res) {
+              if(err) return reject(err)
+              if(res.status != 200) return reject(res.toError())
+              resolve(res.body)
+            })
+          })
       }
     }
   , snapshot: {
-      get: function(id, cb) {
-        request
-        .get(root_url+'/api/v1/snapshots/'+id)
-        .set('Authorization', 'token '+API_key)
-        .end(function loadDocument(err, res) {
-          if(err) return cb(err)
-          if(res.status != 200) return cb(res.toError())
-          cb(null, res.body)
+      get: function(id) {
+        return new Promise(function(resolve, reject) {
+          request
+          .get(root_url+'/api/v1/snapshots/'+id)
+          .set('Authorization', 'token '+API_key)
+          .end(function loadDocument(err, res) {
+            if(err) return reject(err)
+            if(res.status != 200) return reject(res.toError())
+            resolve(res.body)
+          })
         })
       }
     }
@@ -148,12 +174,14 @@ module.exports = function(root_url, API_key) {
 }
 
 module.exports.authenticate = function(root_url, method, credentials) {
-  request
-  .post(root_url+'/token')
-  .send({grant_type: method, credentials: credentials})
-  .end(function() {
-    if(err) return cb(err)
-    if(res.status != 200) return cb(res.toError())
-    cb(null, res.body)
+  return new Promise(function(resolve, reject) {
+    request
+    .post(root_url+'/token')
+    .send({grant_type: method, credentials: credentials})
+    .end(function() {
+      if(err) return reject(err)
+      if(res.status != 200) return reject(res.toError())
+      resolve(res.body)
+    })
   })
 }
